04b7d210fc8faa52c8a5e172d6d2571648cc55d4
Marsking001 <125700072+Marsking001@users.noreply.github.com>
2023-06-10 06:32:55 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import UserInfo from './Components/UserInfo';
+
+class App extends Component{
+  state = {
+    preson: [
+      {FirstName: "Michael"},
+      {LastName: "Adejoorin"},
+      {EmailAddress:"michealade4@gmail.com"},
+      {PhoneNumber: "07037304957"},
+    ],
+    person: [
+      {FirstName: "John"},
+      {LastName: "Micah"},
+      {EmailAddress:"Micxxy@gmail.com"},
+      {PhoneNumber: "+223568596"},
+    ],
+    person: [
+      {FirstName: "John"},
+      {LastName: "Wick"},
+      {EmailAddress:"evilman@gmail.com"},
+      {PhoneNumber: "+4443568596"},
+    ],
+     
+        
+  };
+render(){
+  return (
+    <div className="App">
+UserInfo = 
+    </div>
+  );}
+}
+
+export default App;
