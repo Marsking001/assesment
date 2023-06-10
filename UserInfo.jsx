cd1f1a776183b3d5a24f3ead59a493775173d3df
Marsking001 <125700072+Marsking001@users.noreply.github.com>
2023-06-10 06:48:42 -0700

    Add files via upload

---
+++ UserInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserInfo = (props) => {
+  return (
+    <div>
+      <h1>First Name:{}</h1>
+      <h1>Last Name:{}</h1>
+      <h1>Email:{}</h1>
+      <h1>Phone number:{}</h1> 
+
+    </div>
+  )
+}
+
+export default UserInfo
